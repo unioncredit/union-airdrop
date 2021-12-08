83599f0321e123356ef0c0c5a0cec480b8a9a327
Gerald <me@jacobford.co.uk>
2021-12-08 15:16:33 +0000

    feat: merge snapshot and calculate

---
+++ mergeSnapshots.js
@@ -0,0 +1,79 @@
+const fs = require("fs");
+const path = require("path");
+const { uniq } = require("lodash");
+const { Parser } = require("json2csv");
+
+function readJsonFile(path) {
+  return JSON.parse(fs.readFileSync(path));
+}
+
+function uniqTrust(data, address, propertyA, propertyB) {
+  const res = data
+    .map(
+      (item) =>
+        item[propertyA].toLowerCase() === address.toLowerCase() &&
+        item[propertyB]
+    )
+    .filter(Boolean);
+
+  return uniq(res);
+}
+
+function calculateUnionTokens(data) {
+  let value = 1000;
+
+  if (data.isDefaulted) value -= 500;
+
+  if (data.isRingOwner) value += 1000;
+
+  value += data.borrowers * 100;
+
+  value += data.stakers * 100;
+
+  return value;
+}
+
+const addresses = readJsonFile("./data/polygon-snapshot.json");
+
+const defaulted = readJsonFile("./data/polygon-is-overdue-snapshot.json");
+
+const trustUpdates = readJsonFile("./data/polygon-update-trust-snapshot.json");
+
+const ringOwners = readJsonFile("./data/ring-nft-snapshot.json");
+
+const results = [];
+
+for (const address of addresses) {
+  const isDefaulted = defaulted[address];
+
+  const isRingOwner = ringOwners[address];
+
+  const uniqueBorrower = uniqTrust(trustUpdates, address, "borrower", "staker");
+
+  const uniqueStakers = uniqTrust(trustUpdates, address, "staker", "borrower");
+
+  const data = {
+    address,
+    isDefaulted,
+    isRingOwner,
+    borrowers: uniqueStakers.length,
+    stakers: uniqueBorrower.length,
+  };
+
+  results.push({
+    ...data,
+    tokens: calculateUnionTokens(data),
+  });
+}
+
+// save CSV
+const fields = Object.keys(results[0]);
+const opts = { fields };
+const parser = new Parser(opts);
+const csv = parser.parse(results);
+
+const csvPath = path.resolve("./data/union-tokens.csv");
+
+fs.writeFileSync(csvPath, csv);
+
+console.log(".csv saved to:", csvPath);
