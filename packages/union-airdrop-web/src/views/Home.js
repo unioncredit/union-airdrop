7e0df2002e8accd6ab27e0a50d67bddbde5800fb
Gerald <me@jacobford.co.uk>
2021-12-14 16:54:22 +0000

    fix: tweak home page

--- packages/union-airdrop-web/src/views/Home.js
+++ packages/union-airdrop-web/src/views/Home.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import { ButtonRow, Button, Box, Heading, Text, Label } from "union-ui";
+import { ReactComponent as RightArrow } from "union-ui/lib/icons/arrowRight.svg";
 
 export default function Home() {
   return (
     <Box direction="vertical" align="center" justify="center">
       <video width="80px" autoPlay loop muted controls={false}>
-        <source src="/token.mp4" type="video/mp4" />
+        <source src="/token_400.mp4" type="video/mp4" />
       </video>
       <Heading align="center" mt="24px">
         UNION Governance Token
@@ -14,9 +15,12 @@ export default function Home() {
         $UNION is voting power and voice for the Union protocol. Tokens are
         claimable by early participants of Union’s protocol.
       </Text>
-      <Label as="a" color="blue500">
-        Intro to Union Governance
-      </Label>
+      <Box align="center" justify="center" className="governanceLink">
+        <Label as="a" color="blue500" m={0}>
+          Intro to Union Governance
+        </Label>
+        <RightArrow width="24px" />
+      </Box>
       <ButtonRow fluid maxw="310px" direction="vertical" mt="24px">
         <Link to="/claim" style={{ width: "100%" }}>
           <Button fluid label="Check elegibility" />
