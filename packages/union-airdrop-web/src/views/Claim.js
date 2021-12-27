d58f622e8e030b15c83396dad124822fb182bb54
Gerald <me@jacobford.co.uk>
2021-12-27 21:54:31 +0000

    feat: Claim geo block US

--- packages/union-airdrop-web/src/views/Claim.js
+++ packages/union-airdrop-web/src/views/Claim.js
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
 import { Box } from "union-ui";
+import { useNavigate } from "react-router-dom";
 import ClaimCard from "../components/ClaimCard";
 
+const urls = {
+  production: "https://app.union.finance/api/geo",
+  local: "http://localhost:3000/api/geo",
+};
+
 export default function Claim() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function geoCheck() {
+      try {
+        const resp = await fetch(urls.production);
+        const data = await resp.json();
+        if (data.country === "US") {
+          navigate("/geo");
+        }
+      } catch (_) {}
+    }
+
+    geoCheck();
+  }, []);
+
   return (
     <Box direction="vertical" align="center" justify="center" fluid>
       <ClaimCard />
