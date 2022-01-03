c08732ff91b9eed0f5d998b9c38e600dfa633202
Gerald <me@jacobford.co.uk>
2022-01-03 14:43:25 +0000

    feat: geo location block

--- packages/union-airdrop-web/src/views/Claim.js
+++ packages/union-airdrop-web/src/views/Claim.js
@@ -8,6 +8,36 @@ const urls = {
   local: "http://localhost:3000/api/geo",
 };
 
+/**
+ * Belarus       | BY
+ * Cuba          | CU
+ * Iran          | IR
+ * Iraq          | IQ
+ * Côte d'Ivoire | CI
+ * Libya         | LY
+ * Somalia       | SO
+ * Liberia       | LR
+ * Sudan         | SD
+ * South Sudan   | SS
+ * Syria         | SY
+ * Zimbabwe      | ZW
+ **/
+const restrictedCountryCodes = [
+  "BY",
+  "CU",
+  "IR",
+  "IQ",
+  "CI",
+  "LY",
+  "SO",
+  "LR",
+  "SD",
+  "SS",
+  "SY",
+  "ZW",
+  "US",
+];
+
 export default function Claim() {
   const navigate = useNavigate();
 
@@ -16,8 +46,8 @@ export default function Claim() {
       try {
         const resp = await fetch(urls.production);
         const data = await resp.json();
-        if (data.country === "US") {
-          navigate("/geo");
+        if (restrictedCountryCodes.includes(data.country)) {
+          navigate(`/geo?country=${data.country}`);
         }
       } catch (_) {}
     }
--- packages/union-airdrop-web/src/views/GeoBlock.js
+++ packages/union-airdrop-web/src/views/GeoBlock.js
@@ -1,30 +1,50 @@
+import { useLocation } from "react-router-dom";
 import { Box, Heading, Text, Card, Label } from "union-ui";
 
 export default function GeoBlock() {
+  const props = useLocation();
+
+  const countryCode = props.search
+    .replace("?", "")
+    .split("&")
+    .map((x) => x.split("="))
+    .find((x) => x[0] === "country")?.[1];
+
+  const isUS = countryCode.toLowerCase() === "us";
+
+  const heading = isUS
+    ? "Not available in United States"
+    : "Not available in your location";
+
+  const description = isUS
+    ? "Union’s token distribution is not available for users based in the United States"
+    : "Union’s token distribution is not available for users based in Belarus, Ivory Coast, Cuba, Congo, Iran, Iraq, Liberia, Myanmar, North Korea, Sudan, South Sudan, Syria, Zimbabwe.";
+
   return (
     <Box direction="vertical" align="center" justify="center" fluid>
       <Heading align="center" size="xlarge">
-        Not available in United States
+        {heading}
       </Heading>
       <Text align="center" grey={400}>
-        Union’s token distribution is not available for users based in the
-        United States
+        {description}
       </Text>
-      <Card mt="24px">
-        <Card.Body>
-          <Box justify="center" mb="8px">
-            <img src="/us.png" alt="geo-block-us" width="105px" />
-          </Box>
-          <Heading align="center">Give crypto a home in the U.S.</Heading>
-          <Label as="p" align="center" grey={400}>
-            HODLpac is a political action committee aiming to make sure the
-            cryptoeconomy has a home within the United States
-          </Label>
-          <Label as="p" align="center" color="blue500">
-            Learn more about HODLpac
-          </Label>
-        </Card.Body>
-      </Card>
+      {isUS && (
+        <Card mt="24px">
+          <Card.Body>
+            <Box justify="center" mb="8px">
+              <img src="/us.png" alt="geo-block-us" width="105px" />
+            </Box>
+            <Heading align="center">Give crypto a home in the U.S.</Heading>
+            <Label as="p" align="center" grey={400}>
+              HODLpac is a political action committee aiming to make sure the
+              cryptoeconomy has a home within the United States
+            </Label>
+            <Label as="p" align="center" color="blue500">
+              Learn more about HODLpac
+            </Label>
+          </Card.Body>
+        </Card>
+      )}
     </Box>
   );
 }
